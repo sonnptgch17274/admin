450e42092f5abf56d1f4d0136f89ec68c443f1cc
sonnptgch17274 <sonnptgch17274@fpt.edu.vn>
2019-08-22 17:31:50 +0700

    a

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div><h1>Log in</h1>
+			<form action="process.php" method="Post">
+				<div class="user">
+					<input id=1 type="text" name="username" placeholder="Username" size="50"> <br>
+					<input id=1 type="password" name="password" placeholder="Password" size="50"> <br>
+					<input type="submit" value="Login">
+				</div>
+			</form>	
+    </div>
+</body>
+</html>
