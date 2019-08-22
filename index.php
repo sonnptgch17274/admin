219a95e64e6ca71da5c4a00d2e810815d91cce46
sonnptgch17274 <sonnptgch17274@fpt.edu.vn>
2019-08-22 17:22:48 +0700

    a

--- index.php
+++ index.php
@@ -36,12 +36,21 @@
 			foreach ($resultSet as $row) {
 			echo '<li>' .
 				$row['name'] . ' --' . $row['price'] . ' --' .$row['content'] 
-				. '</li>' . <input type="button" value="Edit">;
+                . '</li>';
+                
 			}
 		?>
 	</ul>
 
-
+    <div><h1>Log in</h1>
+			<form action="process.php" method="Post">
+				<div class="user">
+					<input id="user1" type="text" name="username" placeholder="Username" size="50"> <br>
+					<input id="user1" type="password" name="password" placeholder="Password" size="50"> <br>
+					<input type="submit" value="Login">
+				</div>
+			</form>	
+    </div>
 
 
 
