350bd8467c2d10efe5c061cb56e68e68fb440f6b
sonnptgch17274 <sonnptgch17274@fpt.edu.vn>
2019-08-22 17:58:26 +0700

    a

--- process.php
+++ process.php
@@ -6,15 +6,18 @@
 </head>
 <body>
 <?php 
-require_once './database.php';
-	if($username=="admin" && $password=="admin")
-	{
-		echo "<h1>Login successfully!</h1>";
-		echo "User: " . $username . "<br>";
-		echo "Pass: " . $password . "<br>";
-	}
-	else
-        echo "<h1> Mày nhập sai mật khẩu hoặc tài khoản rồi thèn ngu</h1>";
+require_once './database.php'; 
+function usernameCheck($username) {
+    $stmt = $con->prepare("SELECT username FROM users WHERE username = ':name'");
+    $stmt->bindParam(':name', $username);
+    $stmt->execute();
+
+    if($stmt->rowCount() > 0){
+        echo "exists!";
+    } else {
+        echo "non existant";
+    }
+}
 ?>
 
 </body>
