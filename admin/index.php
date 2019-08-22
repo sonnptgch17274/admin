f2b6aa495d51b293d94eb4ccffd025cdaaaff8be
sonnptgch17274 <sonnptgch17274@fpt.edu.vn>
2019-08-22 16:59:13 +0700

    Initial commit

---
+++ admin/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+	<?php  
+		// $pdo = new PDO('pgsql:host=localhost;port=5432;dbname=GWCourses', 'postgres', '');
+		// echo "done";
+
+		$db = parse_url(getenv("DATABASE_URL"));
+		$pdo = new PDO("pgsql:" . sprintf(
+		    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+		    $db["host"],
+		    $db["port"],
+		    $db["user"],
+		    $db["pass"],
+		    ltrim($db["path"], "/")
+		));
+		
+
+
+		$sql = "SELECT * FROM registercourse";
+		$stmt = $pdo->prepare($sql);
+		//Thiết lập kiểu dữ liệu trả về
+		$stmt->setFetchMode(PDO::FETCH_ASSOC);
+		$stmt->execute();
+		$resultSet = $stmt->fetchAll();
+				
+	?>
+	<ul>
+		<?php  
+			foreach ($resultSet as $row) {
+			echo '<li>' .
+				$row['studentname'] . ' --' . $row['course'] . ' --' .$row[dob] 
+				. '</li>';
+			}
+		?>
+	</ul>
+
+
+
+
+
+</body>
+</html>
