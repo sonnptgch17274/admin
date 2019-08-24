9131c223239bcbdda0153eb9105ca3e916a8616c
sonnptgch17274 <sonnptgch17274@fpt.edu.vn>
2019-08-24 10:59:15 +0700

    a

---
+++ Add.php
@@ -0,0 +1,17 @@
+<?php
+    include "database.php";
+    if(isset($_POST['Register'])){
+        $id = $_POST["txtId"];
+        $name = $_POST["txtName"];
+        $price = $_POST["txtPrice"];
+        $content = $_POST["txtContent"]
+
+        $stsm = $pdo->prepare("INSERT INTO product(id, name, 
+        price, content) VALUES (?,?,?,?)");
+        $stsm->bind_param("ssss", $id, $name, $price, $content);
+        $stsm->execute();
+        $stsm->close();  
+    }
+
+
+    ?>
