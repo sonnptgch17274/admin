6fe1657e1104ea267e4dffd276b9b314448fd4f6
sonnptgch17274 <sonnptgch17274@fpt.edu.vn>
2019-08-24 11:13:09 +0700

    a

--- Add.php
+++ Add.php
@@ -1,15 +1,20 @@
 <?php
     include "database.php";
         // $id = $_POST["txtId"];
+        $id = $_POST["txtId"];
         $name = $_POST["txtName"];
         $price = $_POST["txtPrice"];
         $content = $_POST["txtContent"];
 
-        $stsm = $pdo->prepare("INSERT INTO product(name, 
-        price, content) VALUES (?,?,?)");
-        $stsm->bind_param("sss", $name, $price, $content);
-        $stsm->execute();
-        $stsm->close();  
+        // $stsm = $pdo->prepare("INSERT INTO product(name, 
+        // price, content) VALUES (?,?,?)");
+        // $stsm->bind_param("sss", $name, $price, $content);
+        // $stsm->execute();
+        // $stsm->close();  
+
+        $sql = "INSERT INTO users (id, name, price, content) VALUES (?,?,?,?)";
+        $stmt= $pdo->prepare($sql);
+        $stmt->execute([$id, $name, $price, $content]);
 
 
 
