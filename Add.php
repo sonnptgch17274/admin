e51a61d00868a8c9db9365508b28db5ec60632e8
sonnptgch17274 <sonnptgch17274@fpt.edu.vn>
2019-08-24 10:25:28 +0700

    a

---
+++ Add.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Thank you</title>
+  </head>
+<body>
+    <h1>Thank you for registering the course</h1>
+    <?php
+    include "database.php";
+        $name = $_POST["txtName"];
+        $price = $_POST["txtPrice"];
+        $content = $_POST["txtContent"]
+
+        $stsm = $conn->prepare("INSERT INTO product(name, 
+        price, content) VALUES (?,?,?)");
+        $stsm->bind_param("sss", $name, $price, $content);
+        $stsm->execute();
+        $stsm->close();
+
+    ?>
+
+
+
+
+</body>
+</html>
---
+++ product_editing.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Add product</title>
+  </head>
+<body>
+    <h1>Add product information</h1>
+    <form action="Add.php" method="post">
+        <table width="50%" border="0">
+            <tr>
+                <td>Name</td>
+                <td><input type="text" name ="txtName"></td>
+            </tr>
+
+            <tr>
+                <td>Price</td>
+                <td><input type="text" name ="txtPrice"></td>
+            </tr>
+
+            <tr>
+                <td>Content</td>
+                <td><input type="text" name ="txtContent"></td>
+            </tr>
+
+            <tr>
+                <td></td>
+                <td><input type="submit" name="Register"></td>
+            </tr>
+
+        </table>
+    </form>
+
+</body>
+</html>
