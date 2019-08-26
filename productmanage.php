6bf3e6d98a12f0f8dec65eb1223303f8bbb4f832
sonnptgch17274 <sonnptgch17274@fpt.edu.vn>
2019-08-26 08:49:47 +0700

    a

--- productmanage.php
+++ productmanage.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Manage Product</title>
-    <link rel="stylesheet" href="productmanage1.css">
+    <link rel="stylesheet" href="productmanage.css">
 </head>
 <body>
     <div class="trang">
